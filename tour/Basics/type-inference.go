cc49babee1410d2caa31a8ca0ad0cf2659638873
kojakatsuma <katsuma.koja@ubiregi.com>
2020-04-04 11:37:59 +0900

    first commit

---
+++ tour/Basics/type-inference.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	f := 0.56
+	var i = 4i
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("i is of type %T\n", i)
+	fmt.Printf("f is of type %T\n", f)
+
+}
